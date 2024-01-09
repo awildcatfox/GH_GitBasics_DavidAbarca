f5a504d293be835641dcbae4bcfdabae7583bdeb
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2024-01-09 13:32:00 -0800

    new message added

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Proffessor!");
+            Console.WriteLine("My name is David Abarca and I want to build in Comp science because I think I can make interesting programs and make enough money to survive.");
+
         }
     }
 }
