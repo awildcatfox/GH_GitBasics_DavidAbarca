ac272af43c20835390b1ae4e4b30078c79802ecf
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2024-01-09 13:26:00 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GH_GitBasics_DavidAbarca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
